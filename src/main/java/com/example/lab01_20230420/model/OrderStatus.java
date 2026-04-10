4c5650261467a60059268e402317fdb4f57ef04c
Moreno <moreno@example.com>
2026-04-09 20:48:32 -0500

    ultmos cambios ahora si, (porfa no me bajan 5 no complianm mis opcines de platos XDDDD)

--- src/main/java/com/example/lab01_20230420/model/OrderStatus.java
+++ src/main/java/com/example/lab01_20230420/model/OrderStatus.java
@@ -1,11 +1,11 @@
 package com.example.lab01_20230420.model;
 
 public class OrderStatus {
-    private String nameStatus;
-    public String getNameStatus(){
-        return this.nameStatus;
+    private String status;
+    public String getStatus() {
+        return status;
     }
-    public void setNameStatus(String nameStatus){
-        this.nameStatus = nameStatus;
+    public void setStatus(String status) {
+        this.status = status;
     }
 }
